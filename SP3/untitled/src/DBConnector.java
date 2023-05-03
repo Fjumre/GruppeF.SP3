3033431f0594c95a30d18ce287453671ad2a4904
Yolo-83 <cph-yc123@cphbusiness.dk>
2023-05-03 12:12:35 +0200

    .

--- SP3/untitled/src/DBConnector.java
+++ SP3/untitled/src/DBConnector.java
@@ -25,26 +25,86 @@ public class DBConnector {
 
             //STEP 3: Execute a query
             System.out.println("Creating statement...");
-            String sql = "SELECT name FROM sp3.film WHERE id BETWEEN 1 and 100";
+            String sql = "SELECT ID FROM sp3.film";
+            String sql1 = "SELECT Name FROM sp3.film";
+            String sql2 = "SELECT Year FROM sp3.film";
+            String sql3 = "SELECT Categories FROM sp3.film";
+            String sql4 = "SELECT Rating FROM sp3.film";
+            String sql5 = "SELECT * FROM sp3.film";  /// Den her måde kan læse alle kolonner kaldt "Full List"
+
 
             stmt = conn.prepareStatement(sql);
+            stmt = conn.prepareStatement(sql1);
+            stmt = conn.prepareStatement(sql2);
+            stmt = conn.prepareStatement(sql3);
+            stmt = conn.prepareStatement(sql4);
+            stmt = conn.prepareStatement(sql5); /// Til alle kolonner
+
 
             ResultSet rs = stmt.executeQuery(sql);
+            ResultSet rs1 = stmt.executeQuery(sql1);
+            ResultSet rs2 = stmt.executeQuery(sql2);
+            ResultSet rs3 = stmt.executeQuery(sql3);
+            ResultSet rs4 = stmt.executeQuery(sql4);
+            ResultSet rs5 = stmt.executeQuery(sql5); /// Til alle kolonner
 
             //STEP 4: Extract data from result set
             while(rs.next()){
-                //Retrieve by column name
+                //Retrieve by column ID
 
+                String ID = rs.getString("ID");
+                // int population = rs.getInt("");
+                System.out.println(ID);
+            }
+            while(rs1.next()){
+                //Retrieve by column Name
 
                 String name = rs.getString("Name");
                 // int population = rs.getInt("");
                 System.out.println(name);
-
             }
+            while(rs2.next()){
+                //Retrieve by column Year
+
+                String year = rs.getString("Year");
+                // int population = rs.getInt("");
+                System.out.println(year);
+            }
+            while(rs3.next()){
+                //Retrieve by column Categories
+
+                String categories = rs.getString("Categories");
+                // int population = rs.getInt("");
+                System.out.println(categories);
+            }
+            while(rs4.next()){
+                //Retrieve by column Rate
+
+                String rate = rs.getString("Rate");
+                // int population = rs.getInt("");
+                System.out.println(rate);
+            }
+            while(rs5.next()){
+                //Retrieve by column ID
+
+                String fullList = rs.getString("Full List");
+                // int population = rs.getInt("");
+                System.out.println(fullList);
+            }
+
+
+
             //STEP 5: Clean-up environment
             rs.close();
+            rs1.close();
+            rs2.close();
+            rs3.close();
+            rs4.close();
+            rs5.close();
+            
             stmt.close();
             conn.close();
+
         }catch(SQLException se){
             //Handle errors for JDBC
             se.printStackTrace();
